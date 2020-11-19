fb64797cc6633e184474439db78536bba5186b2b
chipyards <noullet@chipyards.com>
2020-11-19 20:56:49 +0100

    prise en compte des etudiants desactives (drop_code) et redoublants (next_school), calcul des effectifs

--- benizusa/modules/Example/ExampleResource.php
+++ benizusa/modules/Example/ExampleResource.php
@@ -1,21 +1,25 @@
 <?php
 /**
+ liste d'eleves par classes aux format Kmer
  */
 
 echo	'<style type="text/css">', "\n",
+	"table.nobo { background-color: #FFF }\n",
 	"table.nobo td { border:0; padding: 6px; }\n",
-	"table.lp {border-collapse:collapse; }\n",
+	"table.lp { width: 100%; border-collapse:collapse; }\n",
 	"table.lp td { border:1px solid black; padding: 2px 4px 2px 6px; }\n",
+	"table.lp2 { width: 99%; float: right; }\n table.lp2 td { width: 11%; text-align: center }\n",
 	'</style>';
 
 // Display main header with Module icon and Program title.
 // DrawHeader( ProgramTitle() );
 
-// provisoire
-$my_school = 1;
-$my_year = 2020;
+$my_school = UserSchool();
+$my_year = UserSyear();
+// params a mettre dans la config du module
 $my_prefix = '2020A0';
-
+$my_place = 'Yaoundé';
+$my_boss = 'Le Principal';
 
 if	( !isset( $_REQUEST['lp_classe'] ) )
 	{
@@ -27,7 +31,7 @@ if	( !isset( $_REQUEST['lp_classe'] ) )
 	while	( $row = @pg_fetch_array( $result, null, PGSQL_ASSOC ) )
 		{
 		// echo '<pre>'; var_dump( $row ); echo '</pre>';
-		$class_names[$row['id']] = $row['short_name'] . ' ' . $row['title'];
+		$class_names[$row['id']] = $row['short_name'] . ' (' . $row['title'] . ')';
 		}
 	// afficher la form
 	echo '<form action="', $_SERVER['REQUEST_URI'], '" method="GET"> ';
@@ -47,14 +51,20 @@ else	{
 		{
 		$class_name = $row['title'];
 		// identifier les eleves inscrits dans cette classe
-		$sqlrequest = 'SELECT student_id FROM student_enrollment WHERE grade_id=' . $lp_classe . ' AND syear=' . $my_year; 
+		$sqlrequest = 'SELECT student_id, drop_code, next_school FROM student_enrollment WHERE grade_id=' . $lp_classe . ' AND syear=' . $my_year; 
 		// echo $sqlrequest;
 		$result = db_query( $sqlrequest, true );
+		// des arrays tous indexes par le meme index arbitraire
 		$my_students = array();
+		$my_redoub = array();
 		while	( $row = @pg_fetch_array( $result, null, PGSQL_ASSOC ) )
 			{
 			// echo '<pre>'; var_dump( $row ); echo '</pre>';
-			$my_students[] = (int)$row['student_id'];
+			if	( !$row['drop_code'] )
+				{
+				$my_students[] = (int)$row['student_id'];
+				$my_redoub[] = (int)$row['next_school'];
+				}
 			}
 		// echo '<pre>'; var_dump( $my_students ); echo '</pre>';
 		// des tableaux tous indexes par student_id, ainsi si on trie l'un on trie les autres
@@ -62,6 +72,8 @@ else	{
 		$dates_naissance = array();
 		$sexes = array();
 		$statuses = array();
+		// les comptages
+		$cntGN = 0; $cntFN = 0; $cntN = 0; $cntGR = 0; $cntFR = 0; $cntR = 0; $cntG = 0; $cntF = 0;
 		// remplir les tableaux
 		foreach	( $my_students as $k => $v ) {
 			$sqlrequest = 'SELECT first_name, middle_name, last_name, custom_200000000, custom_200000004 '
@@ -71,10 +83,26 @@ else	{
 				{
 				$noms_complets[$v] = $row['last_name'] . ' ' . $row['first_name'] . ' ' . $row['middle_name'];
 				$dates_naissance[$v] = $row['custom_200000004'];
-				if	( $row['custom_200000000'][0] == 'F') $sexes[$v] = 'F';
-				else if	( $row['custom_200000000'][0] == 'M') $sexes[$v] = 'G';
+				if	( $row['custom_200000000'][0] == 'F') { $sexes[$v] = 'F'; $cntF++; }
+				else if	( $row['custom_200000000'][0] == 'M') { $sexes[$v] = 'G'; $cntG++; }
 				else	$sexes[$v] = ' ';
-				$statuses[$v] = 'N';
+				if	( $my_redoub[$k] == 0 )
+					{
+					$statuses[$v] = 'R';
+					$cntR++;
+					if	( $sexes[$v] == 'G' )
+						$cntGR++;
+					else if	( $sexes[$v] == 'F' )
+						$cntFR++;
+					}
+				else	{
+					$statuses[$v] = 'N';
+					$cntN++;
+					if	( $sexes[$v] == 'G' )
+						$cntGN++;
+					else if	( $sexes[$v] == 'F' )
+						$cntFN++;
+					}
 				}
 			}
 		// trier par ordre alphabetique des noms
@@ -86,11 +114,11 @@ else	{
 		echo '<tr><td colspan="2" style="text-align: right"><b>ANNEE SCOLAIRE ', $my_year, '/', $my_year+1, '</b></td></tr>';
 		echo '<tr><td colspan="2" style="text-align: center"><b>LISTE DES ELEVES</b></td></tr>';
 		echo '<tr><td>CLASSE : <b>' . $class_name . '</b></td><td>';
-		echo '<table class="lp" style="width: 50%; float: right"><tr><td colspan="3">NOUVEAUX</td><td colspan="3">REDOUBLANTS</td><td colspan="3">EFFECTIF GENERAL</td></tr>', "\n";
+		echo '<table class="lp lp2"><tr><td colspan="3">NOUVEAUX</td><td colspan="3">REDOUBLANTS</td><td colspan="3">TOTAL</td></tr>', "\n";
 		echo '<tr><td>G</td><td>F</td><td>Total</td><td>G</td><td>F</td><td>Total</td><td>G</td><td>F</td><td>Total</td></tr>', "\n";
-		echo '<tr><td> </td><td> </td><td> </td><td> ',
-		     '</td><td> </td><td> </td><td> </td><td> ',
-		     '</td><td>', count($my_students), '</td></tr>', "\n";
+		echo "<tr><td>$cntGN</td><td>$cntFN</td><td>$cntN</td>",
+		         "<td>$cntGR</td><td>$cntFR</td><td>$cntR</td>",
+			 "<td>$cntG</td><td>$cntF</td><td>", count($my_students), "</td></tr>\n";
 		echo "</table>\n";
 		echo '</td></tr><tr><td colspan="2">';
 		echo '<table class="lp"><tr><td>N°</td><td>MATRICULE</td><td>NOMS ET PRENOMS</td><td>DATE DE NAISSANCE</td><td>SEXE</td><td>STATUT</td></tr>',
@@ -106,6 +134,7 @@ else	{
 			$cnt++;
 			}
 		echo "</table></td></tr>\n";
+		echo '<tr><td>', $my_place, ', le</td><td style="text-align: right">', $my_boss, '</td></tr>';
 		echo "</table>";
 		}
 	}
